dde7d323f8fcb7ec53fab46bdaff37e65187a4a3
ljk4268 <ljk4268@kakao.com>
2022-04-25 22:01:02 +0900

    메뉴리스트페이지/메뉴리스트렌더링

--- js/pages/MenuPage.js
+++ js/pages/MenuPage.js
@@ -165,41 +165,78 @@ export default class MenuPage extends View {
 <!-- //메뉴카테고리영역 -->
 
 <!-- 메뉴리스트영역 -->
-<div class="menu-list-area is-closed">
+${this.menuGroups.map((menuGroup)=>(
+    html`
+    <div class="menu-list-area">
 
     <div class="common-inner">
         <div class="menu-category">
-            <p class="title">새로 나온 메뉴</p>
+            <p class="title">${menuGroup.categoryName}</p>
+            <button class="btn-toggle">
+            <img 
+            class="ico-arrow" 
+            src="../assets/images/ico-arrow.svg"
+            alt=""
+            />
+            </button>
         </div>
+
+
         <ul class="menu-list">
-            <li class="menu-item">
-                <a href="./detail.html" class="menu-detail">
+        ${menuGroup.items.map(item => html`
+        <li class="menu-item">
+                <a class="menu-detail">
                     <div class="menu-img-area">
-                        <img src="https://via.placeholder.com/100x110/ffffff/0000000" alt="{메뉴명}"
-                            class="menu-img" width="100" height="110">
+                        <img class="menu-img" 
+                        src="${item.imageUrl}"
+                        alt="${item.name}"
+                        width="100" height="110">
                     </div>
                     <div class="menu-info-area">
                         <p class="menu-name-group">
-                            <span class="menu-name">메뉴이름</span>
-                            <img src="../assets/images/ico-new.svg" alt="new" class="ico-new">
+                            <span class="menu-name">${item.name}</span>
+                            ${item.isPopular 
+                            ? html `<span class="badge-popular">인기</span>`:''}
+                            <span class="badge-popular">인기</span>
+                            ${item.isNew ? html `
+                            <img 
+                            src="../assets/images/ico-new.svg" 
+                            alt="new" 
+                            class="ico-new"
+                            />` 
+                            : ''}
                         </p>
                         <div class="menu-info-group">
-                            <span class="menu-grade"><img class="ico-star" src="../assets/images/ico-star.svg"
-                                    alt="">5.00</span>
-                            <span class="menu-number-of-order">주문수<em>999</em></span>
+                            <span class="menu-grade">
+                            <img 
+                            class="ico-star" 
+                            src="../assets/images/ico-star.svg"
+                            alt="점수" />
+                            ${item.reviewPoint}</span>
+                            <span class="menu-number-of-order">주문수<em>${item.orderCount}</em></span>
                         </div>
-                        <p class="menu-desc">메뉴에 대한 간단한 설명이 적혀있습니다 메뉴에 대한 간단한 설명이 적혀있습니다</p>
-                        <p class="menu-price">9,999원</p>
-                    </div>
-                </a>
-                <a href="#" class="btn-cart">
-                    <img class="ico-cart" src="../assets/images/ico-cart-fill-green.svg" alt="주문하기">
+                        <p class="menu-desc">${item.description}
+                        </p>
+                        <p class="menu-price">${item.price}원</p>
+                    </div> 
+                    ${item.soldOut
+                    ? html `<a class="btn-cart disabled" >품절</a>` : 
+                    html `</a>
+                <a class="btn-cart">
+                    <img 
+                    class="ico-cart" 
+                    src="../assets/images/ico-cart-fill-green.svg" alt="주문하기"
+                    />
                     <span class="num">0</span>
                 </a>
-            </li>
+            </li>`}
+        `)}
+            
         </ul>
     </div>
-</div>
+</div>`
+))}
+
 `
     }
 }
